9ef386d53999da2d1d281a54d5179708e2066818
Samuel <samuelbeltran048@gmail.com>
2025-03-03 23:49:45 -0500

    Se intento en el parcial MAS dificil :(

--- Libro.java
+++ Libro.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Libro {
     String titulo, autor;
     int ejemplares, ejemplares_prestados;
@@ -45,30 +46,42 @@ public class Libro {
         this.ejemplares_prestados = ejemplares_prestados;
     }
 
-    public boolean prestamo(int ejemplares, int ejemplares_prestados) {
-        if (ejemplares_prestados > ejemplares) {
-            System.out.println("No hay ejemplares disponibles!");
-            
-        } else if (ejemplares_prestados <= ejemplares) {
-            ejemplares -= ejemplares_prestados;
+    public boolean prestamo() {
+        if (ejemplares_prestados < ejemplares) {
+         ejemplares_prestados += ejemplares;
             return true;
-            
-        }
+        } 
                 return false;
     }
 
-    public boolean devolucion(int ejemplares_prestados) {
+    public boolean devolucion() {
         if (ejemplares_prestados > 0) {
-            ejemplares += ejemplares_prestados;
-            ejemplares_prestados -= ejemplares_prestados;
+            ejemplares -= ejemplares_prestados;
             return true;
-        } else {
-            System.out.println("Número de ejemplares a devolver no válido!");
-            return false;
         }
+        return false;
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Libro libro1 = new Libro("El señor de los anillos ", "J.R.R. Tolkien", 10, 0);
 
+        Libro libro2 = new Libro();
+        System.out.println("Ingrese el título del libro: ");
+        libro2.setTitulo(sc.nextLine());
+
+        System.out.println("Ingresar el nombre del autor: ");
+        libro2.setAutor(sc.nextLine());
+
+        System.out.println("Ingresar el numero de ejemplares: ");
+        libro2.setEjemplares(sc.nextInt());
+        System.out.println(" ");
+        System.out.println("Prestamo del libro " + libro1.prestamo());   
+        System.out.println("Devolucion del libro " + libro1.devolucion());
+        System.out.println("");
+        System.out.println("El segundo libro se llama: " + libro2.getTitulo());
+        System.out.println("El autor se llama: " + libro2.getAutor());
+        System.out.println("Tiene " + libro2.getEjemplares() + " ejemplares");
+        
     }
 }
